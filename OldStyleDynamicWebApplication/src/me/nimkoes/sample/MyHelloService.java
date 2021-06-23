7388f783752851076af287703863e0fec8530172
nimkoes <xxxelppa@gmail.com>
2021-06-24 00:20:20 +0900

    1.2 Spring IoC Container 연동

---
+++ OldStyleDynamicWebApplication/src/me/nimkoes/sample/MyHelloService.java
@@ -0,0 +1,12 @@
+package me.nimkoes.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyHelloService {
+
+    public String getName() {
+        return "nimkoes";
+    }
+
+}
--- OldStyleDynamicWebApplication/src/me/nimkoes/sample/MyServlet.java
+++ OldStyleDynamicWebApplication/src/me/nimkoes/sample/MyServlet.java
@@ -7,28 +7,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
 public class MyServlet extends HttpServlet {
 
-	@Override
-	public void init() throws ServletException {
-		System.out.println("MyServlet init !!!");
-	}
+    @Override
+    public void init() throws ServletException {
+        System.out.println("MyServlet init !!!");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.
-	 * HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("MyServlet doGet called !!!");
-		req.getRequestDispatcher("/WEB-INF/view/MyHello.jsp").forward(req, resp);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.
+     * HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-	@Override
-	public void destroy() {
-		System.out.println("MyServlet destroy !!!");
-	}
+        ApplicationContext context = (ApplicationContext) getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+
+        MyHelloService myHelloService = context.getBean(MyHelloService.class);
+
+        System.out.println("MyServlet doGet called !!! >> " + myHelloService.getName());
+
+        // req.getRequestDispatcher("/WEB-INF/view/MyHello.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("MyServlet destroy !!!");
+    }
 
 }
